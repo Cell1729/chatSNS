47e6749a3472977a53b0d5d77af31a0b8307b396
YYYY <ysaq3424@gmail.com>
2025-02-26 18:28:44 +0900

    update : ログイン機能の実装
    画面のリファクタリング

--- users/views.py
+++ users/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-# 暫定機能
-# ユーザー登録画面
-# ユーザーログイン画面
-# ユーザーログアウト
-
-# ログアウトしたらログイン画面にリダイレクト
-# ログインしたらchat画面にリダイレクト
-# ユーザー登録したらログイン画面にリダイレクト
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('chat')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
 
 def logout(request):
-    return render(request, 'login.html')
+    auth_logout(request)
+    return redirect('login')
